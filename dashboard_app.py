b57daea271a0d5910bce63418f658ad34a5e3b1f
wachmaciej <158290588+wachmaciej@users.noreply.github.com>
2025-02-05 16:49:33 +0000

    Update dashboard_app.py

--- dashboard_app.py
+++ dashboard_app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.write('Hello World')
+
+
+col1, col2 = st.columns([3, 1])
+with col1:
+    st.title("YOY Dashboard")
+with col2:
+    st.image("C:\\Users\\MaciejWach\\OneDrive - AFG Media Ltd\\Desktop\\streamlit\\logo.png", width=300)
